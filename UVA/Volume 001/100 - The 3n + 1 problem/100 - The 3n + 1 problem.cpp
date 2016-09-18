681c6089d29d9a52df617cffe8adadd23f9e058f
SAKIB RAHMAN <srskb15@gmail.com>
2016-09-19 01:00:17 +0600

    UVA

--- UVA/Volume 001/100 - The 3n + 1 problem/100 - The 3n + 1 problem.cpp
+++ UVA/Volume 001/100 - The 3n + 1 problem/100 - The 3n + 1 problem.cpp
@@ -2,11 +2,36 @@
 using namespace std;
 
 int main(){
-    int T;
-    
-<<<<<<< HEAD
-    
+    long  a,b,count,m,i,j,temp,temp_i,temp_j;
+    while(cin>>a>>b){
+        temp_i=a;
+        temp_j=b;
+        if(b<a){
+            temp=a;
+            temp_i=b;
+            temp_j=temp;
+        }
+        count=0;
+        for(i=temp_i;i<=temp_j;i++){
+            m=0;
+            j=i;
+            while(j!=0){
+                if(j==1){
+                    m++;
+                    break;
+                }
+                else if(j%2!=0)
+                    j=3*j+1;
+                else
+                    j=j/2;
+
+                m++;
+            }
+
+            if(m>count)
+                count=m;
+        }
+        cout<<a<<" "<<b<<" "<<count<<endl;
+    }
+    return 0;
 }
-=======
-}
->>>>>>> 53a13faaf33ad6ef64ed17ec3dda6bf8ac85253e
