617c9684ddbb4310cf8760ad610be56483d00701
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-15 03:48:05 +0600

    Codeforces

---
+++ Codeforces/486A - Calculating Function.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,b;
+    cin>>a;
+    if(a%2)
+    {
+        b= a/2 + 1;
+        cout<<b*(-1)<<endl;}
+    else
+    {
+        cout<<a/2<<endl;
+    }
+}
