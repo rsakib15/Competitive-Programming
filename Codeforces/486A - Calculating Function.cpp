01e691c1297e391a2a5779ceaa00574991ab264c
SAKIB RAHMAN <rsakib15@users.noreply.github.com>
2016-10-15 03:49:19 +0600

    Update 486A - Calculating Function.cpp

--- Codeforces/486A - Calculating Function.cpp
+++ Codeforces/486A - Calculating Function.cpp
@@ -1,15 +1,13 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     long long a,b;
     cin>>a;
-    if(a%2)
-    {
+    if(a%2){
         b= a/2 + 1;
-        cout<<b*(-1)<<endl;}
-    else
-    {
+        cout<<b*(-1)<<endl;
+    }
+    else{
         cout<<a/2<<endl;
     }
 }
