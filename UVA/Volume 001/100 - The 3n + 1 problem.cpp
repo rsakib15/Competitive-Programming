a0178dcf2be8dcd1f2895e15942337f0b81084f0
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-27 07:50:21 +0600

    UVA

--- UVA/Volume 001/100 - The 3n + 1 problem.cpp
+++ UVA/Volume 001/100 - The 3n + 1 problem.cpp
@@ -1,39 +1,99 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+///*************************************************************///
+#define NL              printf("\n")
+#define nl              '\n'
+#define XX              first
+#define YY              second
+#define SF              scanf
+#define PF              printf
+#define MOD             1000000007
+#define EPS             1e-9
+#define MP              make_pair
+#define PB              push_back
+#define PPB             pop_back
+#define PI              acos(-1.0)
+#define INF             1<<30
+///*************************************************************///
+#define READ()          freopen("input.txt", "r", stdin)
+#define WRITE()         freopen("output.txt", "w", stdout)
+#define BOOST           std::ios_base::sync_with_stdio(0);
+#define MEM(n, val)     memset((n), val, sizeof(n))
+#define DEBUGI(x)       printf("Debug  %d !!\n",x)
+#define DEBUGD(x)       printf("Debug  %llf !!\n",x)
+#define DEBUGL(x)       printf("Debug  %lld !!\n",x)
+#define PC()            printf("Case %d: ", test++)
+///*************************************************************///
+#define ABS(x)          ((x)<0?-(x):(x))
+#define FABS(x)         ((x)+eps<0?-(x):(x))
+#define MAX(a,b)        ((a)>(b)?(a):(b))
+#define MIN(a,b)        ((a)<(b)?(a):(b))
+#define FOR(I,J,K)      for(I = J; I < K; I++)
+#define REV(I,J,K)      for(I = J; I > K; I--)
+#define ssz(a)          strlen(a)
+#define gcd(a,b)        ll _gcd(int a,int b){if(b%a == 0) return a; _gcd(b%a,a);}
+#define lcm(a,b)        ((a*b)/gcd(a,b))
+#define VSORT(v)        sort(v.begin(),v.end())
+#define UNIQUE(V)       (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define DIGIT(x,y)      (((int)(log10((x))/log10((y))))+1)
+#define cmpr            bool compare(const pair<int,int>&i,const pair<int,int>&j){return i.first < j.first;}
+///************************************************************///
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<int, string> pis;
+typedef priority_queue<int> pq;
+///************************************************************///
+template<class T> inline void SWAP(T &a, T &b) { T t = a; a = b; b = t; }
+template<class T> inline T _max(T a, T b){ return (a > b) ? a : b; }
+template<class T> inline T _min(T a, T b){ return (a < b) ? a : b; }
+template<class T> inline void checkmin(T &a, T b){ if (b < a) a = b; }
+template<class T> inline void checkmax(T &a, T b){ if (b > a) a = b; }
+///**************************************************************///
+
+
 int main(){
-    //freopen("input.txt","r",stdin);
-    //freopen("output.txt","w",stdout);
-    long  a,b,count,m,i,j,temp,temp_i,temp_j;
+    //BOOST;
+    //READ();
+    //WRITE();
+
+    int a,b,i,tempi,tempj;
+
     while(cin>>a>>b){
-        temp_i=a;
-        temp_j=b;
-        if(b<a){
-            temp=a;
-            temp_i=b;
-            temp_j=temp;
-        }
-        count=0;
-        for(i=temp_i;i<=temp_j;i++){
-            m=0;
-            j=i;
+        tempi=a;
+        tempj=b;
+
+        if(b<a)
+            SWAP(tempi,tempj);
+        int count=0;
+
+        FOR(i,tempi,tempj+1){
+
+            int m=0;
+            int j=i;
+            //DEBUGI(j);
+
             while(j!=0){
                 if(j==1){
                     m++;
                     break;
                 }
-                else if(j%2!=0)
+                else if(j%2!=0){
                     j=3*j+1;
-                else
+                }
+                else{
                     j=j/2;
-
+                }
                 m++;
             }
-
             if(m>count)
                 count=m;
         }
-        cout<<a<<" "<<b<<" "<<count<<endl;
+
+        PF("%d %d %d\n",a,b,count);
+
     }
-    return 0;
 }
+
