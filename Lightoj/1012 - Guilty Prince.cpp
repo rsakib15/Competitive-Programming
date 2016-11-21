583c55182a0983ab16575e044ad965bf18cb8d9a
SAKIB RAHMAN <rsakib15@users.noreply.github.com>
2016-11-21 17:45:44 +0600

    Update 1012 - Guilty Prince.cpp

--- Lightoj/1012 - Guilty Prince.cpp
+++ Lightoj/1012 - Guilty Prince.cpp
@@ -1,86 +1,123 @@
 #include <bits/stdc++.h>
-#define WHITE 0
-#define GRAY 1
-#define BLACK 2
-#define SIZE 1010
 using namespace std;
 
-char Grid[SIZE][SIZE];
-bool visited[SIZE][SIZE];
-int dx[10]={0,-1,1,0},dy[10]={-1,0,0,1};
+///template by Sakib Rahman///
 
-int BFS(int x,int y,int v,int e){
+///*************************************************************///
+#define NL              printf("\n")
+#define nl              '\n'
+#define XX              first
+#define YY              second
+#define SF              scanf
+#define PF              printf
+#define MOD             1000000007
+#define EPS             1e-9
+#define MP              make_pair
+#define PB              push_back
+#define PPB             pop_back
+#define PI              acos(-1.0)
+#define INF             1<<30
+///*************************************************************///
+#define READ()          freopen("input.txt", "r", stdin)
+#define WRITE()         freopen("output.txt", "w", stdout)
+#define BOOST           std::ios_base::sync_with_stdio(0);
+#define MEM(n, val)     memset((n), val, sizeof(n))
+#define DEBUGI(x)       printf("Debug  %d !!\n",x)
+#define DEBUGD(x)       printf("Debug  %llf !!\n",x)
+#define DEBUGL(x)       printf("Debug  %lld !!\n",x)
+#define PC()            printf("Case %d: ", test++)
+///*************************************************************///
+#define ABS(x)          ((x)<0?-(x):(x))
+#define FABS(x)         ((x)+eps<0?-(x):(x))
+#define MAX(a,b)        ((a)>(b)?(a):(b))
+#define MIN(a,b)        ((a)<(b)?(a):(b))
+#define FOR(I,J,K)      for(I = J; I < K; I++)
+#define REV(I,J,K)      for(I = J; I > K; I--)
+#define ssz(a)          strlen(a)
+#define gcd(a,b)        ll _gcd(int a,int b){if(b%a == 0) return a; _gcd(b%a,a);}
+#define lcm(a,b)        ((a*b)/gcd(a,b))
+#define VSORT(v)        sort(v.begin(),v.end())
+#define UNIQUE(V)       (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define DIGIT(x,y)      (((int)(log10((x))/log10((y))))+1)
+#define cmpr            bool compare(const pair<int,int>&i,const pair<int,int>&j){return i.first < j.first;}
+///************************************************************///
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef pair<int, string> pis;
+typedef priority_queue<int> pq;
+
+
+int fx[]={1,-1,0,0};
+int fy[]={0,0,1,-1};
+
+char Grid[1010][1010];
+int level[1010][1010];
+int visited[1010][1010];
+
+int row,col;
+
+int BFS(int sx,int sy){
+
     int cnt=0;
-    int n1,n2;
+    visited[sx][sy]=1;
+    queue<pii>Q;
+    Q.push(pii(sx,sy));
 
-    queue<int>Q1;
-    queue<int>Q2;
-
-    Q1.push(x);
-    Q2.push(y);
-
-    while(!Q1.empty()){
-        int ux,uy,n1,n2;
-        n1=Q1.front(); Q1.pop();
-        n2=Q2.front(); Q2.pop();
-
-       // cout<<"N1: "<<n1<<" N2: "<<n2<<endl;
-
-        visited[x][y]=GRAY;
+    while(!Q.empty()){
+        pii top=Q.front();
+        Q.pop();
 
         for(int i=0;i<4;i++){
-            ux=n1+dx[i];
-            uy=n2+dy[i];
+            int tx=top.XX + fx[i];
+            int ty=top.YY + fy[i];
 
-        //cout<<"ux  = "<<ux<<" uy = "<<uy<<endl;
-
-        if(ux>=0 && ux<e && uy>=0 && uy<v){
-            if(Grid[ux][uy]=='.' && visited[ux][uy]==WHITE){
-                    Q1.push(ux);
-                    Q2.push(uy);
-                    visited[ux][uy]=GRAY;
-                    cnt++;
-                }
+            if(tx>=0 && tx<col && ty>=0 && ty<row && Grid[tx][ty]=='.' && visited[tx][ty]==0){
+                visited[tx][ty]=1;
+                level[tx][ty]=level[top.XX][top.YY]+1;
+                Q.push(pii(tx,ty));
+                cnt++;
             }
-
         }
-        visited[n1][n2]=BLACK;
-
-
     }
-    while(!Q1.empty())Q1.pop();
-    while(!Q2.empty())Q2.pop();
 
     return cnt;
 }
 
+
+
 int main(){
-    //freopen("input.txt","r",stdin);
-    int T,q=0,W,H,x,y,i,j,xx,yy;
-    scanf("%d",&T);
+    //READ();
+    int node,T,q=0;
+    cin>>T;
     while(T--){
         q++;
-        memset(visited,WHITE,sizeof(visited));
-        memset(Grid,0,sizeof(Grid));
+        MEM(visited,0);
+        MEM(Grid,'+');
+        MEM(level,0);
+
+        int x,y;
         cin>>x>>y;
-        for(i=0;i<y;i++){
+
+        for(int i=0;i<y;i++){
             cin>>Grid[i];
         }
+
+        int xx,yy;
+
         for(int i=0;i<y;i++){
-            for(j=0;j<x;j++)
+            for(int j=0;j<x;j++){
                 if(Grid[i][j]=='@'){
                     xx=i;
                     yy=j;
-
+                }
             }
         }
+        row=x;
+        col=y;
+        int c=BFS(xx,yy);
 
-
-        //cout<<"X= "<<xx<<" Y= "<<yy<<endl;
-        int c=BFS(xx,yy,x,y);
         printf("Case %d: %d\n",q,c+1);
-
-
     }
-
 }
