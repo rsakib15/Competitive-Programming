13a99f0dc6b83a4af12da8500a8ab2695b1bb89d
SAKIB RAHMAN <rsakib15@users.noreply.github.com>
2017-01-09 12:01:44 +0600

    Create BRKTS.cpp
    http://www.spoj.com/problems/BRKTS/

---
+++ Spoj/BRKTS.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    string s;
+    cin>>t;
+    getchar();
+    while(t--){
+        stack<char>st;
+        getline(cin,s);
+        if(s.compare("\n")==0){
+            printf(":)\n");
+            continue;
+        }
+        if(s.size()==1||s.size()%2!=0){
+            printf(":(\n");
+            continue;
+        }
+        int f=0,i;
+        for(i=0;i<s.size();i++){
+            if(s[i]=='{')
+                st.push(s[i]);
+            else if(s[i]=='}' && !st.empty() && st.top()=='{')
+                st.pop();
+            else if(s[i]=='}' && !st.empty() && st.top()=='{')
+                st.pop();
+            else
+                f=1;
+        }
+        if(st.empty()&&f==0)
+            printf(":)\n");
+        else
+        printf(":(\n");
+    }
+}
